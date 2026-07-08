c691859a03296db1eab214b468100d6f305c6197
Ave Fenix Dev <avefenixdev@gmail.com>
2026-07-08 17:02:38 -0300

    Finalizando con el efecto secundario

--- clase-05/01-efecto-secundario/src/pages/Productos.jsx
+++ clase-05/01-efecto-secundario/src/pages/Productos.jsx
@@ -1,7 +1,10 @@
+import { useEffect } from "react"
 
 const Productos = () => {
 
-  
+  useEffect(() => {
+    document.title = 'Educación IT - Productos'
+  }, [])
   
   return (
     <div>Producto</div>
