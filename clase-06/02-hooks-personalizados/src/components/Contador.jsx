a848f01c2135bf7ad00d8b156000534f45ccd336
Ave Fenix Dev <avefenixdev@gmail.com>
2026-07-13 15:05:05 -0300

    Mejoramos la estetica del contador

--- clase-06/02-hooks-personalizados/src/components/Contador.jsx
+++ clase-06/02-hooks-personalizados/src/components/Contador.jsx
@@ -19,10 +19,9 @@ const Contador = ({ valorInicial = 22 }) => {
 
     return (
         <>
-            <h1 className="text-emerald-600">Contador: {contador}</h1>
-            <hr />
-            <button className="bg-red-500 hover:bg-red-700 cursor-pointer rounded px-4 py-2" onClick={handleIncremento}>Incremento</button>
-            <button className="bg-red-500 hover:bg-red-700 cursor-pointer rounded px-4 py-2" onClick={handleDecremento}>Decremento</button>
+            <h3 className="text-emerald-600 text-2xl">Contador: {contador}</h3>
+            <button className="bg-emerald-500 hover:bg-emerald-700 cursor-pointer rounded px-4 py-2 me-2" onClick={handleIncremento}>Incremento</button>
+            <button className="bg-amber-500 hover:bg-amber-700 cursor-pointer rounded px-4 py-2 me-2" onClick={handleDecremento}>Decremento</button>
             <button className="bg-red-500 hover:bg-red-700 cursor-pointer rounded px-4 py-2" onClick={handleReinicio}>Reinicio</button>
         </>
   )
--- clase-06/02-hooks-personalizados/src/pages/CustomHook.jsx
+++ clase-06/02-hooks-personalizados/src/pages/CustomHook.jsx
@@ -1,7 +1,18 @@
+import Contador from "../components/Contador"
 
 const CustomHook = () => {
   return (
-    <div>CustomHook</div>
+    <>
+        <h1 className="text-4xl">Ganchos personalizados (custom hooks)</h1>
+        <hr />
+
+        <h2 className="text-3xl">Componente sin custom hooks</h2>
+
+        <Contador /> 
+
+        <h2>Componente con custom hooks</h2>
+    
+    </>
   )
 }
 
