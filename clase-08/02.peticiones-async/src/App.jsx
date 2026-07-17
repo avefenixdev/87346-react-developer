9caadf55a17ab2159193383cf380c894584086d7
Ave Fenix Dev <avefenixdev@gmail.com>
2026-07-17 16:31:12 -0300

    Variables de entorno en el proyecto

--- clase-08/02.peticiones-async/src/App.jsx
+++ clase-08/02.peticiones-async/src/App.jsx
@@ -1,3 +1,4 @@
+import Productos from "./components/Productos"
 import Usuarios from "./components/Usuarios"
 import useTitulo from "./hooks/useTitulo"
 
@@ -8,6 +9,10 @@ const App = () => {
     <>
       <h1 className="text-yellow-700 text-4xl">Mi app películas!</h1>
       <Usuarios />
+      
+      <hr />
+
+      <Productos />
     </>
   )
 }
---
+++ clase-08/02.peticiones-async/src/components/Productos.jsx
@@ -0,0 +1,13 @@
+
+const Productos = () => {
+
+    const url = 'http://localhost:8080/productos/'
+
+  return (
+    <>
+        <h2 className="text-violet-700 text-5xl font-medium">Listado de productos en una tabla</h2>
+    </>
+  )
+}
+
+export default Productos
