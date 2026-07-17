4a4498ab38ae9be59f76cd0f49414ad023286e7f
Ave Fenix Dev <avefenixdev@gmail.com>
2026-07-17 15:45:59 -0300

    Agregamos spinkit a las peticiones asincronicas

--- clase-08/02.peticiones-async/src/App.jsx
+++ clase-08/02.peticiones-async/src/App.jsx
@@ -1,11 +1,58 @@
+import { useEffect, useState } from "react"
 import useTitulo from "./hooks/useTitulo"
+import Spinner from "./components/Spinner"
 
 const App = () => {
 
+  const [usuarios, setUsuarios] = useState(null)
+
   useTitulo('Buscador de películas')
 
+  const url = 'https://jsonplaceholder.typicode.com/users/'
+
+  const peticionUsuarios = async () => {
+
+    try {
+
+      const res = await fetch(url) // Peticiones asincronica
+      // res -> Tengo el objeto de la respuesta (El modelo de las respuesta de una petición http)
+      // https://developer.mozilla.org/es/docs/Web/API/Response
+
+      if ( !res.ok ) { // ok -> true o false
+        throw new Error('No se pudo obtener los usuarios')
+      }
+
+      const data = await res.json() // text() -> documento de texto | blob() -> recibiendo un binario
+
+      //console.log(data)
+      setUsuarios(data)
+      
+    } catch (error) {
+      console.error(error)
+    }
+
+  }
+
+  useEffect(() => {
+    peticionUsuarios() // esto se ejecuta una sola vez
+  }, [])
+  
+  
+
   return (
+    <>
       <h1 className="text-yellow-700 text-4xl">Mi app películas!</h1>
+      {
+
+        usuarios ? (
+          usuarios.map(element => (
+            <p>{element.name}</p>
+          )))
+        : (
+          <Spinner />
+        )
+      }
+    </>
   )
 }
 
