abccf6bc347f68b388870a37dc7a92897ddc6bbc
Ave Fenix Dev <avefenixdev@gmail.com>
2026-07-22 15:32:33 -0300

    modifico y agrego los elmentos para el contador

--- clase-10/01.redux-sincronico/src/pages/Redux.jsx
+++ clase-10/01.redux-sincronico/src/pages/Redux.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
 
 const Redux = () => {
+
+  const handleIncrementar = () => {
+    
+  }  
+  const handledecrementar = () => {
+    
+  }  
+  const handleIncrementarPorValor = () => {
+    
+  }  
+  
   return (
-    <div>Redux</div>
+    <>
+      <h1 className="text-5xl">Redux</h1>
+      <hr />
+
+      <h2 className="text-2xl">Contador: contador</h2>
+
+      <button 
+        onClick={handleIncrementar} 
+        className="bg-green-500 hover:bg-green-700 p-3 px-4 rounded cursor-pointer me-2 text-white">Incrementar</button>
+      <button 
+        onClick={handledecrementar} 
+        className="bg-yellow-500 hover:bg-yellow-700 p-3 px-4 rounded cursor-pointer me-2 text-white">Decrementar</button>
+      <button 
+        onClick={handleIncrementarPorValor} 
+        className="bg-red-500 hover:bg-red-700 p-3 px-4 rounded cursor-pointer text-white">IncrementarPorValor</button>
+    </>
   )
 }
 
